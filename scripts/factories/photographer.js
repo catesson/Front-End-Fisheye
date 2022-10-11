8cf16c799eb59adb606f0bfcac059cf302463ec8
steven catesson <steven69700@gmail.com>
2022-10-11 14:08:40 +0200

    ajout des fonction de filtre des photographe et des média pour la pages photographe

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -7,7 +7,7 @@ function photographerFactory(data) {
         //déclaration et intégration du contenu des bloc HTML
         const article = document.createElement( 'article' );
         const link = document.createElement( 'a');
-        link.href="/photographer.html?id="+id;
+        link.href="photographer.html?id="+id;
         link.ariaLabel = name;
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
@@ -35,5 +35,37 @@ function photographerFactory(data) {
         
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    function getProfil(){
+        //déclaration et intégration du contenu des bloc HTML
+        const article = document.createElement( 'article' );
+        const link = document.createElement( 'a');
+        link.href="photographer.html?id="+id;
+        link.ariaLabel = name;
+        const img = document.createElement( 'img' );
+        img.setAttribute("src", picture)
+        const h2 = document.createElement( 'h2' );
+        h2.textContent = name;
+        const localisation = document.createElement( 'p' );
+        localisation.textContent = city + ", " + country;
+        const slogan = document.createElement( 'p' );
+        slogan.textContent = tagline;
+        const prix = document.createElement('p')
+        prix.textContent = price + '€/jour' ;
+
+        //ajout des class
+        localisation.className = 'photographer_section--localisation'
+        slogan.className = 'photographer_section--slogan'
+        prix.className = 'photographer_section--prix'
+
+        // liaison au parent 
+        article.appendChild(link);
+        link.appendChild(img);
+        link.appendChild(h2);
+        article.appendChild(localisation);
+        article.appendChild(slogan);
+        article.appendChild(prix);
+        
+        return (article);
+    }
+    return {name, portrait,id,city,country,tagline,price, getUserCardDOM, getProfil}
 }
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,4 +1,49 @@
 //Mettre le code JavaScript lié à la page photographer.html
-// Récupere l'id passer dans le l'url
-var url = new URL(document.location.href)
-var id = url.searchParams.get("id");
+//récupere les données du JSON média et photographe
+async function getPhotographers() {
+    const reponse = await fetch("data/photographers.json");
+    const photographers = await reponse.json();
+    // et bien retourner le tableau photographers seulement une fois
+    return (photographers) 
+}
+
+// filtre les média et les photogaphe
+async function filterMedia(id) {
+    // Récupère les média du photographe dont c'est l'id
+    const { media } = await getPhotographers();
+    console.log(media)
+    const filterMedia = media.filter(item => item.photographerId == id);
+    console.log(filterMedia)
+    return filterMedia
+};
+async function filterPhotographers(id) {
+    // Récupère les datas du photographe dont c'est l'id
+    const { photographers } = await getPhotographers();
+    const filterPhotographe = photographers.filter(item => item.id == id);
+    return filterPhotographe
+};
+
+async function displayData(photographer, media) {
+    //fonction d'affichage des data
+
+    const photographerHeader = document.querySelector(".photograph-header");
+    console.log(photographer)
+    photographer.forEach((photographer) => {
+        const photographerModel = photographerFactory(photographer);
+        console.log(photographerModel)
+        const profilDOM = photographerModel.getProfil();
+        photographerHeader.appendChild(profilDOM);
+    })
+};
+
+async function init() {
+    // Récupere l'id passer dans le l'url
+    var url = new URL(document.location.href)
+    var id = url.searchParams.get("id");
+    // Récupère les datas des photographes
+    const photographer = await filterPhotographers(id)
+    const media = await filterMedia(id)
+    displayData(photographer, media);
+};
+
+init()
